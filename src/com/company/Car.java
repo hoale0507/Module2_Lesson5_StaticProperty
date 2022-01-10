5da1990dcfc9ac4283288b4b4108cdca6d3d49a1
hoale0507 <hoale090293@gmail.com>
2022-01-10 21:29:08 +0700

    add file

---
+++ src/com/company/Car.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Car {
+    private String name;
+
+    private String engine;
+
+
+    public int numberOfCars;
+
+
+    public Car(String name, String engine) {
+
+        this.name = name;
+
+        this.engine = engine;
+//
+//        numberOfCars++;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Car car1 = new Car("Mazda 3","Skyactiv 3");
+
+        System.out.println(car1.numberOfCars);
+
+        Car car2 = new Car("Mazda 6","Skyactiv 6");
+
+        System.out.println(car2.numberOfCars);
+    }
+}
